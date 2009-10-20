71a003d0c967653bb405010dd478a30108a15e6b
Chris Lasher <chris.lasher@gmail.com>
2009-10-20 03:08:21 -0400

    Skeleton of parsers.

---
+++ Bio/GO/Parsers/oboparser.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+A parser for the OBO format.
+
+"""
+
+__author__ = 'Chris Lasher'
+__email__ = 'chris DOT lasher <AT> gmail DOT com'
+
+from Bio.GO import ontology
+
